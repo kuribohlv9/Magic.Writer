21c8362006accfd71c57228481c21231fcf07e61
SemihParlayan <semih.parlayan@hotmail.com>
2015-01-20 13:44:11 +0100

    Added handling for event types

--- Main.cpp
+++ Main.cpp
@@ -9,8 +9,22 @@ int main(int argc, char* argv[])
 	sf::Window window;
 	window.create(sf::VideoMode(1024, 600), "Magic Writer");
 
+	//Game loop
 	while (window.isOpen())
 	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			//Check event types
+
+
+
+			//Close window with X
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
 
 	}
 	return 0;
