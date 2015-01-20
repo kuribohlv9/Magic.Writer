2cfdd9a53df068d3b874ec238435196a80cc93ea
kuribohlv9 <kuribohlv9@gmail.com>
2015-01-20 13:31:29 +0100

    ALL the SFML

--- stdafx.h
+++ stdafx.h
@@ -10,6 +10,23 @@
 #include <stdio.h>
 #include <tchar.h>
 
+//SFLM includes
+#include "SFML\Main.hpp"
+#include "SFML\System.hpp"
+#include "SFML\Window.hpp"
+#include "SFML\Graphics.hpp"
 
+//SFML libs
+#if !defined(NDEBUG)
+#pragma comment(lib, "sfml-main-d.lib")
+#pragma comment(lib, "sfml-system-d.lib")
+#pragma comment(lib, "sfml-window-d.lib")
+#pragma comment(lib, "sfml-graphics-d.lib")
+#else
+#pragma comment(lib, "sfml-main.lib")
+#pragma comment(lib, "sfml-system.lib")
+#pragma comment(lib, "sfml-window.lib")
+#pragma comment(lib, "sfml-graphics.lib")
+#endif
 
 // TODO: reference additional headers your program requires here
