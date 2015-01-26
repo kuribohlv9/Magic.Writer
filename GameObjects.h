f8a896728f3f0657811a0fd017589b53846a1601
Armo0480 <ara.mohammad.94@gmail.com>
2015-01-26 16:30:06 +0100

    GameObjects class (Entity class)

---
+++ GameObjects.h
@@ -0,0 +1,34 @@
+// GameObjects.h
+
+#pragma once
+
+enum GType
+{
+
+};
+
+class GameObjects
+{
+public:
+	GameObjects();
+	virtual ~GameObjects();
+
+protected:
+	// Inside Draw we draw out the sprite using sf::RenderWindow.
+	virtual void Draw(sf::RenderWindow window) = 0;
+	virtual void Update(float deltaTime) = 0;
+	virtual GType GetType() = 0;
+	virtual sf::Sprite* GetSprite();
+	// We were not in need for a collider class as it was a built-in class in SFML, so we just used sf::IntRect.
+	virtual sf::IntRect* GetHitBox();
+	virtual bool IsActive();
+	virtual float GetX();
+	virtual float GetY();
+
+protected:
+	float m_x;
+	float m_y;
+	bool m_active;
+	sf::Sprite* m_sprite;
+	sf::IntRect* m_hitBox;
+};
